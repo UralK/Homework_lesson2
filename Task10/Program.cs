f85fda6b8dbe66f9d3f0e1723aa4f540a6539863
Ural <kilmyhametov@mail.ru>
2022-08-12 20:10:38 +0500

    Task 10 start

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = {"","понедельник","вторник", "среда", "четверг","пятница","суббота","воскресенье"};
+Console.WriteLine("Введите день недели");
+int dayweek = int.Parse(Console.ReadLine()!);
+if (dayweek == 6) 
+{
+
+}
+
+
+
+
+
